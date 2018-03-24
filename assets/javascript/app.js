bf57d077144664a800d1b3ba8e5e4869c86ca22c
Jessica La Torre <jess.latorre@gmail.com>
2018-03-24 14:38:02 -0500

    added two for loops to dynamically add trivia questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,8 +6,7 @@
 //Another array for answer choices
 // third array for answer key.
 //function - write question - then for loop to display questions from questions (only to display on screen)
-//loop to display questions to display questions onto screen.
-onclick function
+//loop to display questions to display questions onto screenonclick function
 //the player will only be able to click one multiple choice answer
 //Will have Finished button at end - need onclick event to show results
 //correct Answers:
@@ -17,8 +16,7 @@ onclick function
 
 $(document).ready(function () {
     // $('.btn-btn-primary-btn-lg').click(function () {
-    alert("test!");
-});
+    console.log("test!");
  
 var questions =
 ["On average, how many aluminum soda cans are used in the United States each year?",
@@ -32,8 +30,17 @@ var questions =
 "How many gallons of motor gasoline does the average American consume in one year?",
 "How much does the average American consume in paper, wood, and other products made from trees each year?",];
 
-var answerChoices =
-[["90 million","80 billion", "120 million", "3 billion"], ["$1 for every $10 spent", "$1 for every $50 spent", "$1 for every $100 spent", "$1 for every $1,000 spent"], [], [], [], [], 
+var answerChoices =[
+["90 million","80 billion", "120 million", "3 billion"],
+["$1 for every $10 spent", "$1 for every $50 spent", "$1 for every $100 spent", "$1 for every $1,000 spent"],
+["Enough to power a microwave for 30 minutes","Enough to power the average American home for 4 days", "Enough to power a computer for 15 minutes", "Enough to power a TV for 3 hours"], 
+["2 times", "0.5 times", "3 times", "None, it takes more energy to recycle"], 
+["20 years","150 years","4000 years", "It will never break down"], 
+["2 hours", "6 hours", "45 minutes","20 minutes"], 
+["Yes", "No", "No idea"],
+["25%","50%","75%","100%"],
+["150 gallons","500 gallons","310 gallons","750 gallons"],
+["1 tree","5 trees","7 trees","15 trees"],
 ];
 
 var answerKey = ["80 billion", "$1 for every $10 spent"];
@@ -42,23 +49,51 @@ var answerKey = ["80 billion", "$1 for every $10 spent"];
 
 function writeQuestion() {
     for(i=0; i< questions.length; i++) {
-    var writeQuestion = $("<br><p>" + questions[i] + "</p></br>"); //closing br tag must go after p tag
+        var addQuestion = $("<div>" + questions[i] + "</div>"); //closing br tag must go after p tag
 
-    // inside our for loo create a class for questions, so we can add a style/attribute to the class. state name of variable first.
-    writeQuestion.addClass("questionStyle");
+    // inside our for loop create a class for questions, so we can add a style/attribute to the class. state name of variable first.
+        addQuestion.addClass("questionStyle");
     
     //now we add an attribute and that is going to be the correct answer to the question
-    writeQuestion.attr("data-question-value", answerKey[i]);
+        addQuestion.attr("data-question-value", answerKey[i]);
 
     //then at some point, we're going to compare user answer with the actual answer (data-question-value variable which will be holding the correct answer). If correc the wins counter will go up.
 
     //now we going to append our question to the questions id onthe html page (make it render). Every time we write a questions, we append (one at a time). If we used write, it wold be replaced each time. Append will add to the list each time the for loop runs.
     //use jquery to add 
-    $('#questionID').append(writeQuestion); //at first the write Question only had a p tag and line break tag; then I added a class. Then I added an attr. Now I'll appending all those details to the DOM: One will be rendered visually; the others will be stored.
+        $('#questionID').append(addQuestion); //at first the write Question only had a p tag and line break tag; then I added a class. Then I added an attr. Now I'll appending all those details to the DOM: One will be rendered visually; the others will be stored.
 
-    //NEXT STEPS: Inside this existing existing for loop, create another for loop with the answers for the answer choice and create the radio buttons. Don't use i use another letter (maybe letter j)
-
-    for(j=0;j<answerChoices; j++);
-    var writeAnswer = $("<p>" + answerChoices[j] + "</p>");
- }
+        // append answer choices
+        // writeAnswerChoicesForQuestion(i);
+    }
+        for (var i = 0; i < answerChoices.length; i++) {
+            for (var j = 0; j < answerChoices[i].length; j++) {
+              var answers = $('#answerChoiceID');
+              var radButton = $('<button>'); //when creating elements for DOM they're inside < >
+              radButton.text(answerChoices[i][j]);
+              answers.append(radButton);
+            }
+          }
 }
+
+// function writeAnswerChoicesForQuestion(indexForQuestion) {
+//     var answerChoicesForQuestion = answerChoices[indexForQuestion];
+//     console.log(answerChoicesForQuestion);
+//     //Jose's example
+//     for (var i = 0; i < answerChoices.length; i++) {
+//         for (var j = 0; j < answerChoices[i].length; j++) {
+//           var answers = $('#answers');
+//           var radButton = $('<button>'); //when creating elements for DOM they're inside < >
+//           radButton.text(answerChoices[i][j]);
+//           answers.append(radButton);
+        // }
+//       }
+
+    // $('<div><input type="radio" name="answerButton" />' + [j][0] + '</div>');
+    // // 
+    // }
+
+$(".btn-primary").click(writeQuestion);
+
+
+});
